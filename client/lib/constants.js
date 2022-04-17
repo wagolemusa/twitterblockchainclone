13ab69a63aa788f1ce1ee122982ff42de49fcd53
wagolemusa <homiemusa@gmail.com>
2022-04-17 12:18:44 +0300

    Final Touches

--- client/lib/constants.js
+++
@@ -1,5 +0,0 @@
-import contractArtifact from  './ProfileImageNfts.json'
-
-export const contractAddress = '0xc8d7494040BDff7f59cB7255909772C6060f2A65';
-
-export const  contractABI = contractArtifact.abi
